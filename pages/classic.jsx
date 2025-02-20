3509416b2a2339800217cfa7e4cd936994fb3c3d
reshmaashwin <reshma124@gmail.com>
2025-02-20 15:04:10 +0400

    removed unwanted file

--- pages/classic.jsx
+++ pages/classic.jsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import Footer from "../components/footer/page";
 import MovieList from "../components/movieList/page.jsx";
 import NavBar from "../components/navBar/page.jsx";
-import TopRatedMovies from "@/components/topRatedMovies/page";
 import GenreMovies from "@/components/genreMovies/page";
 
 export default function ClassicMovie() {
